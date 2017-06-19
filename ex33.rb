963b7a2df928bea024616e0f9270b5e511db91e3
annalaise <annalaise.gibbons@gmail.com>
2017-06-19 22:44:14 +0800

    amending exericse 33, while loops

--- ex33.rb
+++ ex33.rb
@@ -12,13 +12,3 @@ def number_looping(num, incr)
 end
 
 number_looping(10, 1)
-
-def number_looping2
-  numbers = []
-  (0..6).each do |i|
-    numbers << i
-    puts "At the top i is #{i}"
-    puts "Numbers now: ", numbers
-    puts "At the bottom i is #{i}"
- end
-end
